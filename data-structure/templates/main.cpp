d3bfb9ae2d34dff111c5cf60586e94721dda8a5f
Yang-ZhiHang <2097853195@qq.com>
2024-11-16 21:53:04 +0800

    fix: 修改 main.cpp template

--- data-structure/templates/main.cpp
+++ data-structure/templates/main.cpp
@@ -1,17 +1,7 @@
 #include <iostream>
 using namespace std;
-#include "Queue.h"
-int main() {
-    int n;
-    cin >> n;
-    Queue* Q;
-    Q = initQueue(n);
 
-    for (int i = 0; i < n; i++) {
-        int num;
-        cin >> num;
-        enQueue(Q, num);
-    }
+int main() {
 
     return 0;
 }
